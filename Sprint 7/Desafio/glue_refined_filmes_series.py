8013828a21cc5cdc691dfdf9ba94da8dac685774
Luis Eduardo <eduardosantos6622@gmail.com>
2025-11-14 10:37:45 -0300

    ajuste

--- Sprint 7/Desafio/glue_refined_filmes_series.py
+++ Sprint 7/Desafio/glue_refined_filmes_series.py
@@ -14,9 +14,9 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# ============================================================
+
 # 📌 INPUT TRUSTED
-# ============================================================
+
 trusted_movies_path = "s3://data-lake-luis/Trusted/Local/PARQUET/Movies/"
 trusted_series_path = "s3://data-lake-luis/Trusted/Local/PARQUET/Series/"
 trusted_tmdb_path   = "s3://data-lake-luis/Trusted/TMDB/PARQUET/MoviesSeries/"
@@ -25,19 +25,19 @@ movies_df = spark.read.parquet(trusted_movies_path)
 series_df = spark.read.parquet(trusted_series_path)
 tmdb_df   = spark.read.parquet(trusted_tmdb_path)
 
-# ============================================================
+
 # 🔧 Garantir que movies tenha anotermino
-# ============================================================
+
 movies_df = movies_df.withColumn("anotermino", lit(None).cast("int"))
 
-# ============================================================
+
 # 🔗 Unificação de CSV
-# ============================================================
+
 csv_df = movies_df.unionByName(series_df)
 
-# ============================================================
+
 # 🧹 FILTRAGEM TMDB (sem budget)
-# ============================================================
+
 filtered_tmdb = (
     tmdb_df
     .filter(array_contains(col("genre_ids"), 53) | array_contains(col("genre_ids"), 27))
@@ -46,17 +46,17 @@ filtered_tmdb = (
     .filter(year(col("release_date")).between(2012, 2022))
 )
 
-# ============================================================
+
 # 🔗 JOIN
-# ============================================================
+
 refined_df = (
     filtered_tmdb.alias("tmdb")
     .join(csv_df.alias("csv"), col("tmdb.id") == col("csv.id"), "left")
 )
 
-# ============================================================
+
 # 🧱 SELEÇÃO FINAL (somente colunas existentes)
-# ============================================================
+
 final_df = (
     refined_df.select(
         col("tmdb.id"),
@@ -76,9 +76,9 @@ final_df = (
     .withColumn("dia", dayofmonth(col("release_date")))
 )
 
-# ============================================================
+
 # 💾 OUTPUT
-# ============================================================
+
 refined_path = "s3://data-lake-luis/Refined/FilmesSeries/"
 
 (
